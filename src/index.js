d2d7afd08adaef18a3a9a75e918ca7a5e59e35db
sailortushkan <sailortushkan@yandex.ru>
2020-04-07 16:31:23 +0500

    feat: add firebase context

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+import Firebase, { FirebaseContext } from './components/Firebase'
 
 ReactDOM.render(
-  <App />,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
